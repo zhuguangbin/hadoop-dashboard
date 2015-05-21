4265d22df22f695685938fa480ce7a42a43e1e83
zhuguangbin <zhu.guangbin86@gmail.com>
2015-05-21 17:09:15 +0800

    sparksqlweb: default save result to tmp table

--- app/controllers/SparkSQLJDBCController.java
+++ app/controllers/SparkSQLJDBCController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import models.SparkSQLHistroy;
+import org.apache.hadoop.util.Shell;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
@@ -19,6 +20,7 @@ import java.io.*;
 import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -101,6 +103,7 @@ public class SparkSQLJDBCController extends Controller {
       errResponse.put("message", "Internal Server Exception: " + e.getMessage());
       return ok(errResponse);
     } finally {
+
       // update history to db
       String savedFilename = histroy.getId() + ".result.json";
       histroy.setFinishTime(new Date());
@@ -109,9 +112,12 @@ public class SparkSQLJDBCController extends Controller {
       histroy.setResultFile(savedFilename);
       histroy.save();
 
-      // save result
-      if (response != null) {
-        saveResult(response, "public/results/" + savedFilename);
+      // not saving to tmp table, save it locally
+      if (!save) {
+        // save result
+        if (response != null) {
+          saveResult(response, "public/results/" + savedFilename);
+        }
       }
 
     }
@@ -141,10 +147,10 @@ public class SparkSQLJDBCController extends Controller {
       response.put("retcode", 0);
 
       if (save) {
-        response.put("message", "OK, results saved as table : " + resulttable + ", fetching ...");
+        response.put("message", "OK, results saved as table : " + resulttable);
         response.put("resulttable", resulttable);
       } else {
-        response.put("message", "OK, results are as follows ...");
+        response.put("message", "OK, results are as follows");
         response.put("result", result);
       }
       return response;
@@ -242,7 +248,7 @@ public class SparkSQLJDBCController extends Controller {
     return objectNode;
   }
 
-  public static Result fetchResult(String resulttable) {
+  public static Result fetchResult(String resulttable, String limit) {
 
     ObjectNode response = Json.newObject();
 
@@ -253,13 +259,13 @@ public class SparkSQLJDBCController extends Controller {
       conn = cpds.getConnection();
       stmt = conn.createStatement();
 
-      String sqlexec = "SELECT * FROM " + resulttable;
+      String sqlexec = "SELECT * FROM " + resulttable + " LIMIT " + limit;
       Logger.info("RUNNING SQL : " + sqlexec);
       rs = stmt.executeQuery(sqlexec);
 
       JsonNode result = resultSet2Json(rs);
       response.put("retcode", 0);
-      response.put("message", "OK, results fetched back, showing as follows ...");
+      response.put("message", "OK, results fetched back");
       response.put("result", result);
       return ok(response);
 
@@ -268,18 +274,7 @@ public class SparkSQLJDBCController extends Controller {
       se.printStackTrace();
       Logger.error("SQL Exception: " + se.getMessage());
       String message = se.getMessage();
-      String[] m = message.split(":");
-      String cause = m[0].trim();
-      if ("org.apache.spark.sql.AnalysisException".equals(cause)) {
-        response.put("retcode", 1);
-      } else if ("org.apache.spark.SparkException".equals(cause)) {
-        response.put("retcode", 2);
-      } else if ("org.apache.hadoop.hive.ql.metadata.HiveException".equals(cause) || "org.apache.thrift.transport.TTransportException".equals(cause) || "org.apache.thrift.TApplicationException".equals(cause)) {
-        response.put("retcode", 3);
-      } else {
-        // unknown exception
-        response.put("retcode", 100);
-      }
+      response.put("retcode", -1);
 
       response.put("message", se.getMessage());
 
@@ -299,6 +294,12 @@ public class SparkSQLJDBCController extends Controller {
       } catch (SQLException e) {
         Logger.error("SQL Exception: " + e.getLocalizedMessage());
         e.printStackTrace();
+      }
+
+      // save result
+      if (response != null) {
+        String execId = resulttable.split("_")[1];
+        saveResult(response, "public/results/" + execId + ".result.json");
       }
     }//end try
 
@@ -320,6 +321,68 @@ public class SparkSQLJDBCController extends Controller {
       } catch (Exception ex) {
       }
     }
+  }
+
+  public static Result downloadAllResults(String execId){
+    response().setContentType("application/x-download");
+    response().setHeader("Content-disposition", "attachment; filename=result_" + execId + ".tsv");
+    File resultfile = new File(Play.application().path(),"public/results/"+execId+".result.all");
+    if (!resultfile.exists()){
+      fetchHiveData("tmp.result_"+execId,resultfile.getAbsolutePath());
+    }
+    if (resultfile.exists()) {
+      return ok(resultfile);
+    }else {
+      return ok("ERROR, File Not Found");
+    }
+  }
+
+  private static void fetchHiveData(final String table, final String resultfile) {
+
+    List<JsonNode> result = new ArrayList<JsonNode>();
+
+    String sql = "set hive.cli.print.header=true;select * from "+table;
+
+    Shell.ShellCommandExecutor shExec = null;
+    // Setup command to run
+    String[] command = {"/opt/hive/bin/hive", "-e", "\""+sql+"\""};
+
+    Logger.info("executing command: " + Arrays.toString(command));
+    try {
+
+      shExec = new Shell.ShellCommandExecutor(command, new File("/tmp"));
+      shExec.execute();
+
+      int exitCode = shExec.getExitCode();
+      Logger.warn("Exit code from command is : " + exitCode);
+      String message = shExec.getOutput();
+      System.out.println(message);
+
+      Writer writer = null;
+
+      try {
+        writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(resultfile), "utf-8"));
+        writer.write(message);
+      } catch (IOException ex) {
+        // report
+        ex.printStackTrace();
+        Logger.warn("error saving result to file : " + resultfile + " , " + ex.getMessage());
+      } finally {
+        try {
+          writer.close();
+        } catch (Exception ex) {
+        }
+      }
+
+    } catch (IOException e) {
+
+      int exitCode = shExec.getExitCode();
+      String message = shExec.getOutput();
+      Logger.error("IOException when running command : " + Arrays.toString(command) + ", exitcode is " + exitCode + ", " + e.getMessage() + ", message: " + message);
+
+    }
+
   }
 
 }
